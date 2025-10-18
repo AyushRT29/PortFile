f0003e26e23ee3e1f95ba0ff3889b2a7093b4ee1
Ayush <114756439+AyushRT29@users.noreply.github.com>
2025-10-18 23:58:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1 to 10 print using for loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {  
+//         console.log(i);
+//     }
+// }
+
+// for (let i=10 ; i>=1; i--) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 3 == 0) {
+//         console.log(i);
+// } 
+// }
+
+// let a;
+// console.log(a); //undefined
